cf5e2e3601117886ca4e403450748c9e9a541049
cxo26om <cody_owen1@homedepot.com>
2022-08-25 16:26:48 -0400

    Running Two Concurrent Threads

--- src/com/skillsoft/concurrency/MyThread.java
+++ src/com/skillsoft/concurrency/MyThread.java
@@ -11,12 +11,24 @@ public class MyThread implements Runnable{
                 e.printStackTrace();
             }
 
-            System.out.println("i: " + i);
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " says: " + i);
         }
     }
 
     public static void main(String[] args){
-        Thread myThread = new Thread(new MyThread());
-        myThread.start();
+        Thread firstThread = new Thread(new MyThread(), "1st Thread");
+        Thread secondThread = new Thread(new MyThread());
+        secondThread.setName("2nd Thread");
+
+        firstThread.start();
+        secondThread.start();
+
+        //This will throw an IllegalThreadStateException, because it isn't legal to start the same thread more than once
+        //firstThread.start();
+
+        //This will NOT throw an IllegalThreadStateException, because calling the run() method directly causes it to be
+        //called in the CURRENT thread, rather than first starting up a NEW thread
+        //firstThread.run();
     }
 }
