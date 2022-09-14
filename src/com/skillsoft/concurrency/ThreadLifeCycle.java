3e8d00e79ac622f8e284dd722867b1d18e0a0b7c
cxo26om <cody_owen1@homedepot.com>
2022-09-14 17:00:28 -0400

    Initial commit

--- src/com/skillsoft/concurrency/ThreadLifeCycle.java
+++ src/com/skillsoft/concurrency/ThreadLifeCycle.java
@@ -1,2 +1,52 @@
-package com.skillsoft.concurrency;public class ThreadLifeCycle {
+package com.skillsoft.concurrency;
+
+public class ThreadLifeCycle {
+
+    public static class Walk implements Runnable {
+
+        @Override
+        public void run() {
+            for(int i = 1; i < 5; i++){
+                try{
+                    Thread.sleep(3000);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+                System.out.println("I'm walking...");
+            }
+        }
+    }
+
+    public static class ChewGum implements Runnable {
+
+        @Override
+        public void run() {
+            for(int i = 1; i < 5; i++){
+                try{
+                    Thread.sleep(3000);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+                System.out.println("I'm chewing gum...");
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Thread walkThread = new Thread(new Walk());
+        Thread gumThread = new Thread(new ChewGum());
+
+        try{
+            walkThread.start();
+            walkThread.join(5000);
+            gumThread.start();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+    }
 }
