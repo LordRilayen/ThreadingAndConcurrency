6a7645b88c97a4e532314961fabf5ca25dfb373e
cxo26om <cody_owen1@homedepot.com>
2022-09-22 13:21:21 -0500

    The isAlive Method

--- src/com/skillsoft/concurrency/ThreadLifeCycle.java
+++ src/com/skillsoft/concurrency/ThreadLifeCycle.java
@@ -14,7 +14,7 @@ public class ThreadLifeCycle {
                     e.printStackTrace();
                 }
 
-                System.out.println("I'm walking...My state is " + Thread.currentThread().getState());
+                System.out.println("I'm walking...My isAlive state is " + Thread.currentThread().isAlive());
             }
         }
     }
@@ -50,36 +50,55 @@ public class ThreadLifeCycle {
 //
 //    }
 
+//    public static void main(String[] args){
+//        Thread walkThread = new Thread(new Walk());
+//
+//        System.out.println("State of WalkThread after init: " + walkThread.getState());
+//        System.out.println("State of main thread after walkthread init: " + Thread.currentThread().getState());
+//
+//        try {
+//            walkThread.start();
+//            System.out.println("State of WalkThread after start: " + walkThread.getState());
+//            System.out.println("State of main thread after walkthread stat: " + Thread.currentThread().getState());
+//
+//            Thread.sleep(1000);
+//            walkThread.join(5000);
+//
+//
+//            walkThread.join();
+//            System.out.println("State of WalkThread after join: " + walkThread.getState());
+//            System.out.println("State of main thread after walkthread join: " + Thread.currentThread().getState());
+//
+//            System.out.println("Main thread will sleep for 20s...:");
+//            Thread.sleep(20000);
+//
+//            System.out.println("State of WalkThread at the end: " + walkThread.getState());
+//            System.out.println("State of main thread at the end: " + Thread.currentThread().getState());
+//
+//
+//        }
+//        catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
+//
+//    }
+
     public static void main(String[] args){
         Thread walkThread = new Thread(new Walk());
+        System.out.println("IsAlive state of walkThread after init: " + walkThread.isAlive());
 
-        System.out.println("State of WalkThread after init: " + walkThread.getState());
-        System.out.println("State of main thread after walkthread init: " + Thread.currentThread().getState());
-
-        try {
+        try{
             walkThread.start();
-            System.out.println("State of WalkThread after start: " + walkThread.getState());
-            System.out.println("State of main thread after walkthread stat: " + Thread.currentThread().getState());
+            System.out.println("isAlive state of walkThread after start: " + walkThread.isAlive());
 
-            Thread.sleep(1000);
             walkThread.join(5000);
+            System.out.println("isAlive state of walkThread after join: " + walkThread.isAlive());
 
-
-            walkThread.join();
-            System.out.println("State of WalkThread after join: " + walkThread.getState());
-            System.out.println("State of main thread after walkthread join: " + Thread.currentThread().getState());
-
-            System.out.println("Main thread will sleep for 20s...:");
-            Thread.sleep(20000);
-
-            System.out.println("State of WalkThread at the end: " + walkThread.getState());
-            System.out.println("State of main thread at the end: " + Thread.currentThread().getState());
-
-
+            Thread.sleep(10000);
+            System.out.println("isAlive state of walkThread at the end: " + walkThread.isAlive());
         }
         catch (InterruptedException e){
             e.printStackTrace();
         }
-
     }
 }
